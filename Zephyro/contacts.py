9c037ccd409f6f14acba010ccaacb683d8a8e141
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-09-17 13:54:18 +0200

    personnummer initial, contacts initial

---
+++ Zephyro/contacts.py
@@ -0,0 +1,92 @@
+# Uppgift
+#   Skapa ett enkelt telefonboks-program enligt specifikationer.
+
+# Specifikationer
+#   1. Varje post ska innehålla namn, telefonnummer, epostadress och ålder
+#   2. När användare startar programmet så ska hen skriva in ett namn. Då ska det finnas tre fall:
+#       (a) Namnet finns -> Visa uppgifter
+#       (b) Namnet finns inte -> Skriv in uppgifter
+#       (c) Namnet lämnas tomt -> Avsluta programmet
+#   3. När en användare visat eller skrivit in en post så ska ett nytt namn efterfrågas
+#   4. Om en epostadress inte innehåller minst en punkt och precis ett '@' så ska ett nytt efterfrågas
+#   5. Telefonnummer ska sparas som strängar som bara innehåller siffror
+#   6. Ålder ska lagras som ett heltal (integer)
+#   7. Programmet måste byggas enligt principen om att varje funktion endast gör en sak, eftersom det kan komma att
+#      byggas ut senare
+#   8. Av samma skäl som ovan så ska all hantering av exceptions ligga i huvud- funktionen
+
+# Utförande
+#   1. Börja med att skriva huvudloopen, men skriv det med funktionsnamn som inte finns.
+#   2. Gå ett steg nedåt i taget genom att implementera(skriva) funktionerna som behövs
+#   3. Tips på datastruktur: En dict med namn som nycklar, där varje post är en dict med fältnamn som nycklar
+
+import re
+
+
+class EmailError(Exception):
+    pass
+
+
+class PhoneNumberError(Exception):
+    pass
+
+
+class Entry(object):
+
+    def __init__(self, name: str, number: int, email: str) -> None:
+        self.name = name
+        self.number = number
+        self.email = email
+
+    def __str__(self) -> str:
+        text = (
+            f'   Name: {self.name.rjust(10)}\n'
+            f'   Number: {str(self.number).rjust(10)}\n'
+            f'   Email: {self.email.rjust(10)}\n')
+        return text
+
+
+def input_email(retries: int = 3) -> str:
+    email = input('Enter email\n>>> ')
+    match = re.match(r'[^@]+@[^@]+.[^@]+', email)
+    print(match)
+    if not match:
+        if not retries:
+            raise EmailError('Invalid email adress! must contain exactly one `@` and at least one `.`')
+        return input_email(retries=retries - 1)
+    return email
+
+
+def input_number(retries: int = 3) -> int:
+    try:
+        number = int(input('Enter phone number:\n>>> ').replace(' ', '').replace('-', ''))
+    except ValueError:
+        if not retries:
+            raise PhoneNumberError('Invalid phone number! must be digits')
+        return input_number(retries=retries - 1)
+    return number
+
+
+def main():
+    book = {}
+    while True:
+        try:
+            name = input('Enter contact name\n>>> ')
+        except Exception as e:
+            print(e)
+        if not name:
+            break
+        if name in book:
+            print(book[name])
+        else:
+            try:
+                number = input_number()
+                email = input_email()
+            except Exception as e:
+                print(e)
+            else:
+                book[name] = Entry(name, number, email)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Zephyro/personnummer.py
@@ -0,0 +1,109 @@
+
+def stripCentury(number):
+    """Strips the number of the century
+
+    Sometimes, personnummer is written including century.
+    This is not included in the algorithm for calculating the
+    final digit, and thus is removed by this function."""
+    length = len(number)
+    print(f'stripCentury: length: {length}')
+    if not number.isdigit():
+        print('Error: input is not a number.')
+        return None, False
+    if length == 9:
+        return number, False
+    elif length == 10:
+        return number, True
+    elif length == 11:
+        return number[2:], False
+    elif length == 12:
+        return number[2:], True
+    else:
+        print('invalid format')
+        return None, False
+
+
+def fixFormat(number):
+    """Format the number in a predictable way.
+
+    Fixes the problem with different ways of formatting the number.
+    The resulting format is always yymmddnnn or yymmddnnnn depending on the
+    input."""
+
+    number = number.replace("-", "")
+    number, full = stripCentury(number)
+    print(f'fixFormat: returning number: {number}')
+    return number, full
+
+
+def doubleAndSum(number):
+    """Doubles a number and adds the digits in it
+
+    Takes an integer and doubles it, and then adds the numbers together."""
+
+    print(f'    doubleAndSum: incoming number: {number}')
+    number *= 2
+    print(f'    doubleAndSum: doubled number: {number}')
+    if number >= 10:
+        number = number // 10 + number % 10
+    print(f'    doubleAndSum: returning number: {number}')
+    return number
+
+
+def calculateControlDigit(number):
+    """Calculate the control digit for a personnummer
+
+    The control digit is calculated by multiplying every other number by two, (Starting with the first) then adding the
+    separate digits. The control digit is then the difference
+    between that sum and the closest higher multiple of ten. E.g. if the sum is 44, the control digit will be 50-44=6"""
+
+    cumulative_sum = 0
+
+    # Enumerate makes a tuple of index and element
+    for index, digit in enumerate(number):
+
+        # Make calculations possible
+        digit = int(digit)
+
+        if index % 2 == 0:
+            cumulative_sum += doubleAndSum(digit)
+        else:
+            cumulative_sum += digit
+        print(f'calculateControlDigit: sum: {cumulative_sum}')
+
+    print(f'calculateControlDigit: final sum: {cumulative_sum}')
+    # This is equivalent to taking the following multiple of ten minus the number
+    control = (cumulative_sum // 10 + 1) * 10 - cumulative_sum
+    # higher = int(str(cumulative_sum)[0] + '0') + 10
+    # control = higher - cumulative_sum
+    return control
+
+
+def printControlDigitMatch(number, control):
+    last = int(number) % 10
+    print('')
+    if last == control:
+        print("Personnumrets kontrollsiffra stämmer!")
+    else:
+        print(f"Personnumrets kontrollsiffra stämmer INTE!\nDen ska vara {control}, men är {last}")
+
+# This statement means that the code below will not run if imported.
+# This is good when running automated tests, which we will cover later
+
+
+if __name__ == "__main__":
+
+    number = input("Skriv in ett personnummer du vill testa eller ett ofullständigt som du vill generera:\n>> ")
+    number, full = fixFormat(number)
+    if full:
+        control = calculateControlDigit(number[:-1])
+    elif not full and number:
+        control = calculateControlDigit(number)
+    else:
+        print('invalid number')
+    print(f'main: control digit: {control}')
+
+    if full:
+        printControlDigitMatch(number, control)
+    else:
+        print(f"Det fullständiga personnumret är: {number}{control}")
